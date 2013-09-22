9d41f788bb17fde6c5fdeffbba8f9c4e94ef6a90
lwch <lwch748@gmail.com>
2013-09-22 12:21:43 +0800

    1.修正ngx_lua_debug.h中的一些宏错误 2.修改ngx_lua_debug.h中输出内容的格式

--- src/ngx_lua_debug.h
+++ src/ngx_lua_debug.h
@@ -3,24 +3,26 @@
 
 #include <stdio.h>
 
-#if defined(DDEBUG) && (DDEBUG)
-#   if (NGX_HAVE_VARIADIC_MACROS)
+#if defined(NGX_DEBUG) && (NGX_DEBUG)
+#   if (NGX_HAVE_C99_VARIADIC_MACROS)
 #       define dbg(...) do { \
                             fprintf(stderr, "func: %s\n", __func__); \
                             fprintf(stderr, __VA_ARGS__); \
-                            fprintf(stderr, "\nfile: %s line: %d.\n", __FILE__, __LINE__); \
+                            fprintf(stderr, "file: %s line: %d.\n", __FILE__, __LINE__); \
                         } while (0)
 #   else
 void dbg(const char* fmt, ...)
 {
+    fprintf(stderr, "func: %s\n", __func__);
     va_list l;
     va_start(l, fmt);
     vfprintf(stderr, fmt, l);
     va_end(l);
+    fprintf(stderr, "file: %s line: %d.\n", __FILE__, __LINE__);
 }
 #   endif
 #else
-#   if (NGX_HAVE_VARIADIC_MACROS)
+#   if (NGX_HAVE_C99_VARIADIC_MACROS)
 #       define dbg(...)
 #   else
 void dbg(const char* fmt, ...)
